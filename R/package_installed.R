670a1d2d1eb91f796f86f096fdb543de832b94f7
Richard Aubrey White <hello@rwhite.no>
2025-03-10 08:00:36 +0100

    Update package_installed.R

--- R/package_installed.R
+++ R/package_installed.R
@@ -1,10 +1,22 @@
-#' Is a package installed?
-#' @param pkg Package name.
-#' @returns Boolean. True if package is installed, false if not.
+#' Check if a Package is Installed and Optionally Install it
+#'
+#' This function checks whether a specified package is installed in the current R environment.
+#' Optionally, it can install the package if it is not already installed.
+#'
+#' @param pkg A character string specifying the name of the package to check.
+#' @param install_if_missing A logical value indicating whether to install the package if it is not installed. Default is `FALSE`.
+#' @return A logical value: `TRUE` if the package is installed (or successfully installed), `FALSE` otherwise.
 #' @examples
+#' \dontrun{
 #' org::package_installed("data.table")
+#' org::package_installed("ggplot2", install_if_missing = TRUE)
+#' }
 #' @export
-package_installed <- function(pkg){
+package_installed <- function(pkg, install_if_missing = FALSE){
   pkgs <- utils::installed.packages()[,"Package"]
+  if (!(pkg %in% pkgs) && install_if_missing) {
+    install.packages(pkg)
+    pkgs <- utils::installed.packages()[,"Package"]
+  }
   return(pkg %in% pkgs)
 }
